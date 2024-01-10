3f08e9c1a11c1cdc98270be1f6f5ff3972771c1a
Jon Ethi <jonmethington@gmail.com>
2024-01-10 09:59:53 -0700

    Added the beans item, partially

--- src/main/java/net/jooba/brazilianfoods/JoobasBrazilianFoods.java
+++ src/main/java/net/jooba/brazilianfoods/JoobasBrazilianFoods.java
@@ -1,6 +1,8 @@
 package net.jooba.brazilianfoods;
 
 import com.mojang.logging.LogUtils;
+import net.jooba.brazilianfoods.items.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -27,7 +29,8 @@ public class JoobasBrazilianFoods
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        // Register the commonSetup method for modloading
+        ModItems.register(modEventBus);
+
         modEventBus.addListener(this::commonSetup);
 
         // Register ourselves for server and other game events we are interested in
@@ -48,6 +51,9 @@ public class JoobasBrazilianFoods
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
+        if(event.getTabKey() == CreativeModeTabs.FOOD_AND_DRINKS) {
+            event.accept(ModItems.BEANS);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
